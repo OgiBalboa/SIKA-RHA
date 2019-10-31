0634a5fbe8f2437e6a4a56f30bad2f17c59a4d9a
OgiBalboa <48028006+OgiBalboa@users.noreply.github.com>
2019-10-31 10:34:13 +0300

    Update ogibalboa.py

--- SIKAR-HA Control Panel/ogibalboa.py
+++ SIKAR-HA Control Panel/ogibalboa.py
@@ -1,8 +1,18 @@
 """
-S-HA PANEL gekiştirmeleri için oluşturulmuş modül.
+
 author : @ogibalboa
 date : 20.10.2019
 """
+#-------------------------------------------------------------------------------
+# Name:        ogibalboa
+# Purpose:     S-HA PANEL geliştirmeleri için oluşturulmuş modül.
+#
+# Author:      @ogibalboa
+#
+# Created:     07.10.2019
+# Copyright:   (c) OgiBalboa 2019
+# Licence:     <GNU GCC>
+#-------------------------------------------------------------------------------
 #----------------------------RESİM YÜKLEME FONKSİYONU---------------
 from PIL import Image,ImageTk
 class resim():
